68b3d9665487b9f2fbb2f8fbf8b35a0b5bd62df1
sdonne <stefanodonne@gmail.com>
2020-10-27 11:55:59 +0100

    Add variables print

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 class Main {
 
 	private String sourcePath;
 
-	private ArrayList<Symbol> symbols = new ArrayList<>();
+	private ArrayList<Symbol> variables = new ArrayList<>();
 
 	private LexicalAnalyzer scanner;
+
+	private class CustomComparator implements Comparator<Symbol> {
+		@Override
+		public int compare(Symbol s1, Symbol s2) {
+			return ((String)s1.getValue()).compareTo((String)s2.getValue());
+		}
+	}
 
 	public Main(String sourcePath) {
 		this.sourcePath = sourcePath;
@@ -20,14 +29,31 @@ class Main {
 			System.exit(-1);
 		}
 	}
-
 	void printTable() {
 		try {
 			Symbol symbol = scanner.nextToken();
 			while (symbol != null) {
 				System.out.println(symbol);
+				if (symbol.getType() == LexicalUnit.VARNAME){
+					boolean matched = false;
+					for(Symbol s : variables){
+						if (((String)(s.getValue())).equals((String)(symbol.getValue()))){
+							matched = true;
+						}
+					}
+					if (!matched) {
+						variables.add(symbol);
+					}
+
+				}
 				symbol = scanner.nextToken();
 			}
+			System.out.println("\nVariables");
+			Collections.sort(variables,new CustomComparator());
+			for(Symbol s : variables){
+				System.out.println(s.getValue()+" "+s.getLine());
+			}
+			System.out.println();
 		} catch (Exception e) { // IOException for stream exception or Error for parser exception.
 			e.printStackTrace();
 			System.exit(-1);
