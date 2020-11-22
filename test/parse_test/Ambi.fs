c1bf8848df223ef649fa752bec0d86f3e508f5b0
sdonne <stefanodonne@gmail.com>
2020-11-22 14:44:19 +0100

    Starts adding tests

---
+++ test/parse_test/Ambi.fs
@@ -0,0 +1,3 @@
+BEGINPROG Example
+vartest := 1*1*1
+ENDPROG
--- test/parse_test/parse.java
+++ test/parse_test/parse.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 public class parse {
+	@Test
+	public void testFactorial() throws SyntaxException {
+		Scanner scanner = new Scanner("test/parse_test/Factorial.fs");
+		scanner.printTable();
+		Parser parser = new Parser(scanner.getVariables());
+		parser.parseSequence();
+	}
+	@Test
+	public void testAmbigu() throws SyntaxException {
+		Scanner scanner = new Scanner("test/parse_test/Ambi.fs");
+		scanner.printTable();
+		Parser parser = new Parser(scanner.getVariables());
+		parser.parseSequence();
+	}
+	@Test
+	public void testUnary() throws SyntaxException {
+		Scanner scanner = new Scanner("test/parse_test/Unary.fs");
+		scanner.printTable();
+		Parser parser = new Parser(scanner.getVariables());
+		parser.parseSequence();
+	}
+	@Test
+	public void testNestedWhile() throws SyntaxException {
+		Scanner scanner = new Scanner("test/parse_test/NestedWhile.fs");
+		scanner.printTable();
+		Parser parser = new Parser(scanner.getVariables());
+		parser.parseSequence();
+	}
 }
