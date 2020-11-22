edcec710ed75bb9a698c4a40528fcb8ec9ec9d68
sdonne <stefanodonne@gmail.com>
2020-11-22 14:41:19 +0100

    Starts implementing tests

---
+++ test/parse_test/NestedWhile.fs
@@ -0,0 +1,20 @@
+BEGINPROG Factorial
+WHILE (1 > 0) DO
+    result := 0
+    WHILE (1 > 0) DO
+        result := 0
+        WHILE (1 > 0) DO
+            result := 0
+            WHILE (1 > 0) DO
+                result := 0
+                WHILE (1 > 0) DO
+                    result := 0
+                    WHILE (1 > 0) DO
+                        result := 0
+                    ENDWHILE
+                ENDWHILE
+            ENDWHILE
+        ENDWHILE
+    ENDWHILE
+ENDWHILE
+ENDPROG
