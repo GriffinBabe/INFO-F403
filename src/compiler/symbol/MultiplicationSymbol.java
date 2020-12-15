0c65e6685753eb75d03727cacb4a936e4d609886
sdonne <stefanodonne@gmail.com>
2020-12-15 17:57:25 +0100

    Adds all arith op toLLVM

--- src/compiler/symbol/MultiplicationSymbol.java
+++ src/compiler/symbol/MultiplicationSymbol.java
@@ -6,7 +6,13 @@ public class MultiplicationSymbol extends OperatorSymbol {
 
     @Override
     public String toLLVM(CompilerTable table, String... returnRegisters) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        String elem1 = table.nextRegister();
+        String elem2 = table.nextRegister();
+        sb.append(left.toLLVM(table,elem1)).append(right.toLLVM(table,elem2));
+        sb.append(returnRegisters[0]).append(" = alloca i32\n");
+        sb.append(returnRegisters[0]).append(" = mul i32 ").append(elem1).append(" , ").append(elem2).append("\n");
+        return sb.toString();
     }
 
     @Override
