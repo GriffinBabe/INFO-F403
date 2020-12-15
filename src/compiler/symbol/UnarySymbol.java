512db173f05258be0b491e58a9e8f60312da0a1b
sdonne <stefanodonne@gmail.com>
2020-12-15 18:03:23 +0100

    Adds unary minus toLLVM

--- src/compiler/symbol/UnarySymbol.java
+++ src/compiler/symbol/UnarySymbol.java
@@ -4,11 +4,17 @@ import compiler.CompilerTable;
 
 public class UnarySymbol extends ExpressionSymbol {
 
-    protected ExpressionSymbol left;
+    protected ExpressionSymbol right;
 
     @Override
     public String toLLVM(CompilerTable table, String... returnRegisters) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        String elem1 = table.nextRegister();
+        // determines what's right of the unary minus and multiply it by -1
+        sb.append(right.toLLVM(table,elem1));
+        sb.append(returnRegisters[0]).append(" = alloca i32\n");
+        sb.append(returnRegisters[0]).append(" = mul i32 ").append("-1").append(" , ").append(elem1).append("\n");
+        return sb.toString();
     }
 
     @Override
@@ -16,7 +22,7 @@ public class UnarySymbol extends ExpressionSymbol {
         return "<Unary>";
     }
 
-    public void setLeft(ExpressionSymbol left) {
-        this.left = left;
+    public void setLeft(ExpressionSymbol right) {
+        this.right = right;
     }
 }
